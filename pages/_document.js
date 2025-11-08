c43c829bfa48c0316dca649cbcb1cabe4ff0d761
yameenrafiqi <yameem369@gmail.com>
2025-11-08 17:35:40 +0530

    Add rawspill.svg as favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="alternate icon" href="/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
